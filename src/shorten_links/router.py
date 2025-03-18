81f916297b999d7f21c91a59935d1fd5ebc8a041
KuBaN23_ <garkovski_dmitri@mail.ru>
2025-03-18 18:14:50 +0300

    add use shorten links

--- src/shorten_links/router.py
+++ src/shorten_links/router.py
@@ -11,13 +11,12 @@ from src.database import get_async_session
 from src.shorten_links.schemas import ShortenLinkCreate
 from src.auth.user_manager import current_active_user
 from src.models import User
-from src.shorten_links.crud import add_shorten_link, get_shorten_link
 from src.shorten_links.utils import generate_alias
 
 router = APIRouter()
 
 @router.post("/shorten")
-async def shorten_link(
+async def create_shorten_link(
     shorten_link: ShortenLinkCreate,
     response: Response,
     current_user: Optional[User] = Depends(current_active_user),
@@ -62,12 +61,30 @@ async def shorten_link(
 
 
 @router.get("/{short_code}")
-async def redirect_to_original_url(short_code: str):
-    return await get_shorten_link(short_code)
+async def redirect_to_original_url(
+    short_code: str,
+    response: Response,
+    session: AsyncSession = Depends(get_async_session),
+):
+    query = select(ShortenLink).where(ShortenLink.alias == short_code)
+    result = await session.execute(query)
+    link = result.scalar_one_or_none()
+    if link is None:
+        raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Link not found",
+            )
+    
+    link.clicks += 1
+    last_clicked_at = datetime.now(timezone.utc)
+    last_clicked_at = last_clicked_at.replace(tzinfo=None)
+    link.last_clicked_at = last_clicked_at
+    await session.commit()
+    response.status_code = status.HTTP_307_TEMPORARY_REDIRECT
+    return RedirectResponse(link.url, status_code=307)
 
 async def is_alias_exists(session: AsyncSession, alias: str) -> bool:
     # Выполняем запрос для проверки существования alias
     query = select(ShortenLink).where(ShortenLink.alias == alias)
     result = await session.execute(query)
     return result.scalar() is not None  # Если запись найдена, возвращаем True
-    
