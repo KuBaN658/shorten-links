df34bc3d7b05fb258821c974dc6dd673f8e6dc2e
KuBaN23_ <garkovski_dmitri@mail.ru>
2025-03-23 17:43:25 +0300

    fix remove links

--- src/celery_app/config_celery.py
+++ src/celery_app/config_celery.py
@@ -1,18 +1,16 @@
-import asyncio
 from datetime import datetime, timedelta, timezone
-from sqlalchemy import insert
+from sqlalchemy import insert, select
 from celery import Celery
 from loguru import logger
 
-from models import OldShortenLink
-from database import async_session_maker
-from shorten_links.utils import get_link_by_short_code
+from models import OldShortenLink, ShortenLink
+from database import session_maker
 from config import settings
 
 app = Celery(
     "tasks",  # Имя приложения
     broker=f"redis://{settings.redis.tasks_host}:6379/0", 
-    backend=f"redis://{settings.redis.tasks_host}:6379/0",
+    backend=f"redis://{settings.redis.tasks_host}:6379/1"
 )
 
 app.conf.update(
@@ -30,40 +28,35 @@ app.conf.update(
     time_limit=300,
 )
 def delete_link_if_expired(short_code: str):
-    """
-    Удаляет ссылку и добавляет её в таблицу OldShortenLink.
-    """
+    with session_maker() as session:
+        # Получаем ссылку
+        logger.info("Получаем ссылку")
+        query = select(ShortenLink).where(ShortenLink.alias == short_code)
+        link = session.execute(query).scalar_one_or_none()
+        if link is None:
+            logger.info("Ссылка не найдена")
+            return  # Ссылка уже удалена
 
-    async def _delete_link():
-        async with async_session_maker() as session:
-            # Получаем ссылку
-            logger.info("Получаем ссылку")
-            link = await get_link_by_short_code(session, short_code)
-            if link is None:
-                logger.info("Ссылка не найдена")
-                return  # Ссылка уже удалена
-
-            now = datetime.now(timezone.utc).replace(tzinfo=None)
-            logger.info(f"текущее время: {now}")
-            if link.expires_at <= now or (
-                link.last_clicked_at
-                and (now - link.last_clicked_at)
-                > timedelta(seconds=settings.life_time_links.without_clicks)
-            ):
-                logger.info(f"ссылка просрочена: {link.expires_at}")
-                # Копируем ссылку в OldShortenLink
-                await session.execute(
-                    insert(OldShortenLink).values(
-                        url=link.url,
-                        created_at=link.created_at,
-                        alias=link.alias,
-                        user_id=link.user_id,
-                        deleted_at=now,
-                    )
-                )
-                logger.info("Ссылка удалена")
-                # Удаляем ссылку из основной таблицы
-                await session.delete(link)
-                await session.commit()
-
-    asyncio.run(_delete_link())
+        now = datetime.now(timezone.utc).replace(tzinfo=None)
+        logger.info(f"текущее время: {now}")
+        # if link.expires_at <= now or (
+        #     link.last_clicked_at
+        #     and (now - link.last_clicked_at)
+        #     > timedelta(seconds=settings.life_time_links.without_clicks)
+        # ):
+        logger.info(f"ссылка просрочена: {link.expires_at}")
+        # Копируем ссылку в OldShortenLink
+        session.execute(
+            insert(OldShortenLink).values(
+                url=link.url,
+                created_at=link.created_at,
+                alias=link.alias,
+                user_id=link.user_id,
+                deleted_at=now,
+            )
+        )
+            
+        # Удаляем ссылку из основной таблицы
+        session.delete(link)
+        session.commit()
+        logger.info("Ссылка удалена")
--- src/database.py
+++ src/database.py
@@ -1,5 +1,7 @@
 from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from config import settings
 
@@ -7,8 +9,12 @@ from config import settings
 DATABASE_URL = f"postgresql+asyncpg://{settings.db.user}:{settings.db.password}@"
 DATABASE_URL += f"{settings.db.host}:{settings.db.port}/{settings.db.name}"
 
-engine = create_async_engine(DATABASE_URL)
-async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+async_engine = create_async_engine(DATABASE_URL)
+async_session_maker = async_sessionmaker(async_engine, expire_on_commit=False)
+
+DATABASE_URL = f"postgresql+psycopg2://{settings.db.user}:{settings.db.password}@{settings.db.host}:{settings.db.port}/{settings.db.name}"
+sync_engine = create_engine(DATABASE_URL)
+session_maker = sessionmaker(sync_engine)
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
